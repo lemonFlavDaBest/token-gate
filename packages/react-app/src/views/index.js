fce00e20aa251c222bfe430a489e4f21eb2b4735
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-12-22 10:58:39 -0800

    Update index.js
    add admingate to the exporter

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -4,3 +4,4 @@ export { default as Hints } from "./Hints";
 export { default as Subgraph } from "./Subgraph";
 export { default as SampleNFT } from "./SampleNFT";
 export { default as UserGate } from "./UserGate";
+export { default as AdminGate } from "./AdminGate";
