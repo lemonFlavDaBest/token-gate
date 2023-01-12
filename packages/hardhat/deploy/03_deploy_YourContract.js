56214e9cf0157aeab51f6c65c86b0a2ea8963f01
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-01-11 18:59:04 -0800

    Create 03_deploy_YourContract.js
    adding this to deploy to test with

---
+++ packages/hardhat/deploy/03_deploy_YourContract.js
@@ -0,0 +1,40 @@
+// deploy/00_deploy_your_contract.js
+
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+// const sleep = (ms) =>
+//   new Promise((r) =>
+//     setTimeout(() => {
+//       console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
+//       r();
+//     }, ms)
+//   );
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+  console.log(chainId, "chainId")
+
+  await deploy("YourContractT", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  // Getting a previously deployed contract
+  const YourContract = await ethers.getContract("YourContract", deployer);
+  /*
+  const result = await SampleNFT.transferOwnership(
+    "ADDRESS_HERE"
+   );
+
+   await result.wait()
+*/
+  
+};
+module.exports.tags = ["YourContract"];
